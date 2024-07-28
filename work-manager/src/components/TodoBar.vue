4fe04819235db492a2eab7d4f85ebcd2234d70c3
Terminal123 <138257699+Dark-Ai-py@users.noreply.github.com>
2024-07-28 08:26:41 -0500

    started framing the todo area

---
+++ work-manager/src/components/TodoBar.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+	<div
+		class="flex justify-center rounded-2xl bg-neutral max-w-80"
+		style="height: 80vh"
+	>
+		<h1>frfr</h1>
+		<div class="outline-double outline-red-700"></div>
+		<div class="outline-double outline-blue-700"></div>
+	</div>
+</template>
+
+<style scoped></style>
