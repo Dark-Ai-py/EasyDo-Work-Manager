8ac6c6ffdb55f4318d265cdac9d27a75f2cf5270
Terminal123 <138257699+Dark-Ai-py@users.noreply.github.com>
2024-07-28 17:30:01 -0500

    started sizing things up and creating a wireframe

--- work-manager/src/components/TodoBar.vue
+++ work-manager/src/components/TodoBar.vue
@@ -2,12 +2,15 @@
 
 <template>
 	<div
-		class="flex justify-center rounded-2xl bg-neutral max-w-80"
+		class="flex justify-center rounded-2xl bg-neutral max-w-80 flex-col"
 		style="height: 80vh"
 	>
 		<h1>frfr</h1>
-		<div class="outline-double outline-red-700"></div>
-		<div class="outline-double outline-blue-600"></div>
+		<div class="outline-double outline-red-700 h-20 mb-8"></div>
+		<div
+			class="outline-double outline-blue-600 h-fit"
+			style="max-height: calc(80vh-7rem)"
+		></div>
 	</div>
 </template>
 
