73396ee1f541ee418d1fb8cbbad65da4d529a917
Terminal123 <138257699+Dark-Ai-py@users.noreply.github.com>
2024-07-29 09:17:55 -0500

    Todo Section Update -finished outlining the sections -starting on the props

--- work-manager/src/components/TodoBar.vue
+++ work-manager/src/components/TodoBar.vue
@@ -1,15 +1,17 @@
-<script setup></script>
+<script setup>
+	const { todoItems } = defineProps(["items"]);
+	function ParseProps() {}
+</script>
 
 <template>
 	<div
 		class="flex justify-center rounded-2xl bg-neutral max-w-80 flex-col"
 		style="height: 80vh"
 	>
-		<h1>frfr</h1>
-		<div class="outline-double outline-red-700 h-20 mb-8"></div>
+		<div class="outline-double outline-red-700 h-20 mb-8 mr-4 ml-4"></div>
 		<div
-			class="outline-double outline-blue-600 h-fit"
-			style="max-height: calc(80vh-7rem)"
+			class="outline-double outline-blue-600 h-fit mb-8 mr-4 ml-4"
+			style="height: 100%"
 		></div>
 	</div>
 </template>
